8c0619656738c9a530b2b9ba6971788cb2087174
Piyush-Kaduskar <kaduskarpiyush7@gmail.com>
2026-04-07 11:49:50 +0530

    Update PiyushKTestSIM17.sql

--- models/PiyushKTestSIM17.sql
+++ models/PiyushKTestSIM17.sql
@@ -1,8 +1,11 @@
+{{ config(
+    materialized = 'table',
+    alias = 'PiyushKTestSIM17'     -- This forces the exact name you want
+) }}
+
 select
     emp_id as employee_id,
-
     initcap(emp_name) as full_name,
-
     case
         when department = 'IT' then 'Technology'
         when department = 'HR' then 'Human Resources'
@@ -10,13 +13,10 @@ select
         when department = 'Marketing' then 'Marketing Team'
         else department
     end as dept_name,
-
     case
         when salary >= 75000 then 'High Salary'
         when salary between 65000 and 74999 then 'Medium Salary'
         else 'Standard Salary'
     end as salary_category,
-
     to_varchar(joining_date, 'DD-MON-YYYY') as formatted_joining_date
-
-from "PIYUSHK_DB"."PUBLIC"."EMPLOYEES"
+from "PIYUSHK_DB"."PUBLIC"."EMPLOYEES";
